00c15459366e1eca1cb7b3581a3a7d218ca0c11a
Zkrallah <muhammad.heshamyt@gmail.com>
2024-09-19 14:45:09 +0300

    feat: Clients can get the available tags.

--- src/main/java/com/zkrallah/sanad/controller/ClientsController.java
+++ src/main/java/com/zkrallah/sanad/controller/ClientsController.java
@@ -6,17 +6,26 @@ import static com.zkrallah.sanad.response.ApiResponse.createSuccessResponse;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.zkrallah.sanad.dtos.CreateRequestDto;
 import com.zkrallah.sanad.entity.Lawyer;
 import com.zkrallah.sanad.entity.Request;
+import com.zkrallah.sanad.entity.Tag;
 import com.zkrallah.sanad.response.ApiResponse;
 import com.zkrallah.sanad.response.MessageResponse;
 import com.zkrallah.sanad.service.lawyer.LawyerService;
 import com.zkrallah.sanad.service.request.RequestService;
 import com.zkrallah.sanad.service.storage.StorageService;
+import com.zkrallah.sanad.service.tag.TagService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +39,7 @@ public class ClientsController {
     private final LawyerService lawyerService;
     private final StorageService storageService;
     private final RequestService requestService;
+    private final TagService tagService;
 
     @GetMapping("/lawyers")
     public ResponseEntity<ApiResponse<List<Lawyer>>> getLawyers() {
@@ -111,6 +121,17 @@ public class ClientsController {
             return ResponseEntity.ok(createSuccessResponse(requests));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(createFailureResponse("Could not get requests: " + e.getMessage()));
+        }
+    }
+
+    @GetMapping("/tags")
+    public ResponseEntity<ApiResponse<List<Tag>>> getTags() {
+        try {
+            List<Tag> tags = tagService.getTags();
+            return ResponseEntity.ok(createSuccessResponse(tags));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body(createFailureResponse("Could not get tags: " + e.getMessage()));
         }
     }
 
