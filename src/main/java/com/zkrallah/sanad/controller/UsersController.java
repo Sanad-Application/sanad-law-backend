dc4321d16c5926949d21e6c935fc6d26b2caed14
Zkrallah <muhammad.heshamyt@gmail.com>
2024-09-20 03:47:25 +0300

    fix: The me route is now accessed by JWT instead of user id.

--- src/main/java/com/zkrallah/sanad/controller/UsersController.java
+++ src/main/java/com/zkrallah/sanad/controller/UsersController.java
@@ -6,7 +6,15 @@ import static com.zkrallah.sanad.response.ApiResponse.createSuccessResponse;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.zkrallah.sanad.dtos.UpdateUserDto;
@@ -27,6 +35,17 @@ public class UsersController {
 
     private final UserService userService;
     private final StorageService storageService;
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<User>> getMe(
+            @RequestHeader("Authorization") String authHeader) {
+        try {
+            User user = userService.getUserByJwt(authHeader);
+            return ResponseEntity.ok(createSuccessResponse(user));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(createFailureResponse("Could not get user: " + e.getMessage()));
+        }
+    }
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<User>>> getUsers() {
